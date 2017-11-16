4a4e4b162ab300d8bfcfb6a1cd90723d74a73717
George Puisha <e.puysha@itransition.com>
2017-11-16 18:41:33 +0300

    Add some server tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,32 @@
 const assert = require("assert");
+const request = require("request");
+const app = require("../app.js");
+
+const base_url = "http://localhost:3000/";
 
 describe("Testing test system", () => {
     describe("true === true", () => {
         it("should return true", () => {
             return(true === true);
+        });
+    });
+});
+
+describe("Server", () => {
+    describe("GET /", () => {
+        it("should return status code 200", (done) => {
+                request.get(base_url, (error, response, body) => {
+                assert.equal(200, response.statusCode);
+                done();
+            });
+        });
+
+        it("should return \"Response\"", (done) => {
+            request.get(base_url, (error, response, body) => {
+                assert.equal("Response", body);
+                app.closeServer();
+                done();
+            });
         });
     });
 });
