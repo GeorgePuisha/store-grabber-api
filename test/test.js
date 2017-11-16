4bfc379ce45c35af3d28ba57cec06ecdfc2b8262
George Puisha <e.puysha@itransition.com>
2017-11-16 18:22:11 +0300

    Add simple test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+describe("Testing test system", () => {
+    describe("true === true", () => {
+        it("should return true", () => {
+            return(true === true);
+        });
+    });
+});
