b4e725ecdb0afb52476bca3a30769d9af24ee539
George Puisha <e.puysha@itransition.com>
2017-11-18 18:00:04 +0300

    Server structure

--- app.js
+++ app.js
@@ -2,19 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const { Pool, Client } = require("pg");
-const connectionString = "postgres://user:password@host:port/database";
+const routes = require("./routes/index");
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || connectionString,
-    ssl: true
-});
+app.use("/api", routes)
 
 const server = app.listen(process.env.PORT || 3000);
 
-app.get("/", (req, res) => {
-    res.send("Response");
-});
+app.use(routes);
 
 module.exports.closeServer = () => {
     server.close();
