6304833b841176938b48d4792b85eab212ed9184
George Puisha <e.puysha@itransition.com>
2017-11-18 18:20:49 +0300

    Delete old tests

--- test/test.js
+++ test/test.js
@@ -7,26 +7,7 @@ const url = "http://localhost:3000/";
 describe("Testing test system", () => {
     describe("true === true", () => {
         it("should return true", () => {
-            return(true === true);
-        });
-    });
-});
-
-describe("Server", () => {
-    describe("GET /", () => {
-        it("should return status code 200", (done) => {
-                request.get(base_url, (error, response, body) => {
-                assert.equal(200, response.statusCode);
-                done();
-            });
-        });
-
-        it("should return \"Response\"", (done) => {
-            request.get(base_url, (error, response, body) => {
-                assert.equal("Response", body);
-                app.closeServer();
-                done();
-            });
+            return (true === true);
         });
     });
 });
