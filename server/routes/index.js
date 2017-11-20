54df0bf01bd1cc02d55f6cc968ea2dffe382b2bd
George Puisha <e.puysha@itransition.com>
2017-11-20 17:46:54 +0300

    Fix multiple users notes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,15 +3,25 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/login/", (req, res) => {
-    models.User.create({
-        email: req.body.email,
-        nickname: req.body.nickname
-    }).then((user) => {
+    models.User.findOrCreate({
+        where: {
+            email: req.body.email
+        },
+        defaults: {
+            email: req.body.email,
+            nickname: req.body.nickname
+        }
+    }).then((result) => {
+        const user = result[0];
+        const created = result[1];
+        if (created) {
+            res.status(409);
+        }
         res.json(user);
     });
 });
 
-router.get('/all', function(req, res) {
+router.get('/all/', function(req, res) {
     models.User.findAll({}).then(function(users) {
         res.json(users);
     });
