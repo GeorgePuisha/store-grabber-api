a637db5539553bdd202044155e483a289053f910
George Puisha <e.puysha@itransition.com>
2017-11-20 16:49:42 +0300

    Configure migration

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+    var User = sequelize.define("User", {
+        email: DataTypes.STRING,
+        nickname: DataTypes.STRING
+    }, {
+        classMethods: {
+            associate: function(models) {
+                User.hasMany(models.Todo);
+            }
+        }
+    });
+    return User;
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,9 @@
 const models = require("../models/index");
-
 const express = require("express");
 const router = express.Router();
 
 router.get("/login/:email/:nickname", () => {
-    console.log("Got it!");
+
 });
 
 module.exports = router;
