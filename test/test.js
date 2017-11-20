cc0a2f49904f7f8aecc66536e7924f1182b4aa4f
George Puisha <e.puysha@itransition.com>
2017-11-20 20:02:35 +0300

    Fix tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,5 @@
 const assert = require("assert");
 const request = require("request");
-const app = require("../app.js");
-
-const url = "http://localhost:3000/";
 
 describe("Testing test system", () => {
     describe("true === true", () => {
