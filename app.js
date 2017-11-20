d33e36dbb6a1a05bb81d8d67abbb90f0c2ab0f3a
George Puisha <e.puysha@itransition.com>
2017-11-20 17:16:30 +0300

    Deal with CORS

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const cors = require("cors");
 
 const routes = require("./server/routes/index");
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Content-Type", "application/json");
+    next();
+});
+app.use(cors());
 
 app.use("/api", routes)
 
