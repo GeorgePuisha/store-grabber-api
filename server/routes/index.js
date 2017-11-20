2458051d03664774429c74742ca81b792e7e516b
George Puisha <e.puysha@itransition.com>
2017-11-20 17:16:30 +0300

    Deal with CORS

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,13 @@ const models = require("../models/index");
 const express = require("express");
 const router = express.Router();
 
-router.get("/login/:email/:nickname", () => {
-
+router.post("/login", (req, res) => {
+    models.User.create({
+        email: req.body.email,
+        nickname: req.body.nickname
+    }).then((user) => {
+        res.json(user);
+    });
 });
 
 module.exports = router;
