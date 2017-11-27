38524140b5435c1283c5b0afa5bf378bc8a3054c
George Puisha <33485080+GeorgePuisha@users.noreply.github.com>
2017-11-27 18:40:27 +0300

    Update watch.js

--- server/routes/watch.js
+++ server/routes/watch.js
@@ -31,7 +31,7 @@ const addToWatched = (req, resp) => {
             where: {
                 email: req.params.email
             }
-        }).then((user) => findOrCreateWatched(user, product, resp);)
+        }).then((user) => findOrCreateWatched(user, product, resp));
     });
 };
 
