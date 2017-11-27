25731096aaab7411d8e3c437c5614a6748863b79
George Puisha <e.puysha@itransition.com>
2017-11-27 13:53:05 +0300

    Rename search route file

---
+++ server/routes/onliner.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const needle = require("needle");
+
+const onliner = "https://catalog.api.onliner.by/search/products?query=";
+
+const reduceInformation = (product) => {
+    return {
+        key: product.key,
+        name: product.extended_name,
+        description: product.description,
+        image: product.images.header,
+        price: product.prices.price_min.amount,
+        status: "active"
+    };
+};
+
+const isActive = (product) => {
+    return product.status === "active";
+};
+
+const isAvailable = (product) => {
+    return product.prices !== null;
+};
+
+const createResponse = (products) => {
+    const response = [];
+    products.forEach((product) => {
+        if (isActive(product) && isAvailable(product)) {
+            response.push(reduceInformation(product));
+        }
+    });
+    return response;
+};
+
+
+
+const search = (req, resp) => {
+    const url = onliner + req.params.query + "&page=" + req.params.page;
+    needle.get(url, (err, res, body) => {
+        if (res.body.products !== undefined) {
+            resp.status(200).json(createResponse(res.body.products));
+        }
+    });
+};
+
+const lastPage = (req, resp) => {
+    const url = onliner + req.params.query;
+    needle.get(url, (err, res, body) => {
+        resp.status(200).json(res.body.page.last);
+    });
+};
+
+module.exports.search = search;
+module.exports.lastPage = lastPage;
