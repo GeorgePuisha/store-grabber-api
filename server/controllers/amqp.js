f6ac0dba5d4f4b4a7451c29150882b8061a2d51b
George Puisha <e.puysha@itransition.com>
2017-12-05 18:20:06 +0300

    amqp.js refactoring

--- server/controllers/amqp.js
+++ server/controllers/amqp.js
@@ -2,30 +2,32 @@ const queue = "letters";
 const url = process.env.CLOUDAMQP_URL;
 const open = require("amqplib").connect(url);
 
+const read = (channel, callback) => {
+    channel.assertQueue(queue);
+    channel.consume(queue, (msg) => {
+        if (msg !== null) {
+            callback(JSON.parse(msg.content));
+            channel.ack(msg);
+        }
+    });
+};
+
 const getFromQueue = (callback) => {
     open.then((connection) => {
         let createdChannel = connection.createChannel();
-        createdChannel = createdChannel.then((channel) => {
-            channel.assertQueue(queue);
-            channel.consume(queue, (msg) => {
-                if (msg !== null) {
-                    callback(JSON.parse(msg.content));
-                    channel.ack(msg);
-                }
-            })
-        });
-        return createdChannel;
+        createdChannel = createdChannel.then((channel) => read(channel, callback));
     });
+};
+
+const write = (channel, information) => {
+    channel.assertQueue(queue);
+    channel.sendToQueue(queue, new Buffer(JSON.stringify(information)));
 };
 
 const sendToQueue = (information) => {
     open.then((connection) => {
         let createdChannel = connection.createChannel();
-        createdChannel = createdChannel.then((channel) => {
-            channel.assertQueue(queue);
-            channel.sendToQueue(queue, new Buffer(JSON.stringify(information)));
-        });
-        return createdChannel;
+        createdChannel = createdChannel.then((channel) => write(channel, information));
     });
 };
 
