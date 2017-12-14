e3ecf924c8decf63e724c04a58fcddd8c462d37c
George Puisha <e.puysha@itransition.com>
2017-12-14 15:20:41 +0300

    Handle CORS

--- server/workers/notifications.js
+++ server/workers/notifications.js
@@ -5,6 +5,13 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://store-grabber.herokuapp.com");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(cors());
 
 server.listen(process.env.PORT || 3001);
