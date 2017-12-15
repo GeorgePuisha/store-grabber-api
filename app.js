f2c8b69aa02ec98f40981c9f9cf31e4cf615c54f
George Puisha <e.puysha@itransition.com>
2017-12-15 14:20:50 +0300

    Bug fixes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const routes = require("./server/routes/index");
 const app = express();
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://store-grabber.herokuapp.com");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
