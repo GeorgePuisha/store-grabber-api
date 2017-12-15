da91da5402edfb26fe3d7882176b54e7057636b3
George Puisha <e.puysha@itransition.com>
2017-12-15 13:13:06 +0300

    Revert commits

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
