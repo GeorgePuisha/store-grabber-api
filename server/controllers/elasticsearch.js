65e3c87465bd8a285688f39d99759e55f5641f2d
George Puisha <e.puysha@itransition.com>
2018-01-04 11:48:18 +0300

    Try to fix deploy bug

--- server/controllers/elasticsearch.js
+++ server/controllers/elasticsearch.js
@@ -3,5 +3,6 @@ const elasticsearch = require("elasticsearch");
 const connectionString = process.env.BONSAI_URL;
 
 module.exports = new elasticsearch.Client({
-    host: connectionString
+    host: connectionString,
+    log: "trace"
 });
